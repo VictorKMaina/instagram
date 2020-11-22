a5677a1be1f03b0df67aedbfc622aa3cdc517323
Victor Maina <contact@victormaina.com>
2020-11-22 20:26:28 +0300

    add Image, Comment, and Profile classes to models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Image(models.Model):
+    """
+    Class to define image instances. Inherits from models.Model.
+    """
+    image_name = models.CharField(max_length = 255)
+    image_caption = models.TextField()
+    image_path = CloudinaryField('image')
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.IntegerField()
+
+class Comment(models.Model):
+    """
+    Class to define image instances. Inherits from models.Model.
+    """
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    """
+    Class to define image instances. Inherits from models.Model.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField()
+    bio = models.CharField(max_length = 350)
