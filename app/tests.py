e445522653f66abc5049b06ee3799c49f3fc9fa9
Victor Maina <contact@victormaina.com>
2020-11-22 20:37:35 +0300

    create test classes with docstrings

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from .models import Profile, Image, Comment, User
 
 # Create your tests here.
+class ProfileTest(TestCase):
+    """
+    Class for testing Profile methods and properties. Inherits from TestCase.
+    """
+    def setUp(self):
+        """
+        Runs before each test
+        """
+    
+    def tearDown(self):
+        """
+        Runs after each test
+        """
+
+    def test_instance(self):
+        """
+        Test class to check if self.<object> is instance of <class> Class
+        """
+
+class ImageTest(TestCase):
+    """
+    Class for testing Image methods and properties. Inherits from TestCase.
+    """
+    def setUp(self):
+        """
+        Runs before each test
+        """
+    
+    def tearDown(self):
+        """
+        Runs after each test
+        """
+
+    def test_instance(self):
+        """
+        Test class to check if self.<object> is instance of <class> Class
+        """
+
+class CommentTest(TestCase):
+    """
+    Class for testing Comment methods and properties. Inherits from TestCase.
+    """
+    def setUp(self):
+        """
+        Runs before each test
+        """
+    
+    def tearDown(self):
+        """
+        Runs after each test
+        """
+
+    def test_instance(self):
+        """
+        Test class to check if self.<object> is instance of <class> Class
+        """
